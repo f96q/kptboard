688be4ccf6b488413fa241787293a33da62f56f7
danny <danny.code1@gmail.com>
2014-01-06 02:37:25 +0900

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Kptboard::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'retrospectives#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
