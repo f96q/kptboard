de8bcf5edff57ec7319df71e2a314259c54faf30
danny <danny.code1@gmail.com>
2014-01-13 02:40:16 +0900

    setup websocket-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+//= require websocket_rails/main
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Kptboard::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.middleware.delete Rack::Lock
 end
