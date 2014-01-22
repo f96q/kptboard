2ce14b5f5c6ae2648e42676c69ca7eb84749c575
danny <danny.code1@gmail.com>
2014-01-23 00:40:55 +0900

    add forcus: true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,4 +55,8 @@ RSpec.configure do |config|
   end
 
   config.include FeatureHelper, :type => :feature
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
