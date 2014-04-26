c46ba695d7cb696933fcf7e7233d50830d48f01c
danny <danny.code1@gmail.com>
2014-04-26 20:51:01 +0900

    fix DEPRECATION WARNING

--- app/models/retrospective.rb
+++ app/models/retrospective.rb
@@ -16,5 +16,5 @@ class Retrospective < ActiveRecord::Base
     self.users.pluck(:id).include? user_id
   end
 
-  scope :has_user, -> (user_id) { where(RetrospectiveUser.arel_table[:user_id].eq(user_id)).includes(:retrospective_users) }
+  scope :has_user, -> (user_id) { includes(:retrospective_users).where(RetrospectiveUser.arel_table[:user_id].eq(user_id)).references(:retrospective_users) }
 end
