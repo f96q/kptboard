5158f0fcc30be8fda1e3294c9ee5ba023ca1988a
danny <danny.code1@gmail.com>
2015-02-08 21:40:18 +0900

    fix desin
    fix failed test

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -9,7 +9,7 @@ feature 'session' do
     fill_in 'Name', with: 'test'
     fill_in 'Password', with: 'password'
     fill_in 'Password Confirmation', with: 'password'
-    click_button 'Sign up'
+    find('.retrospective-form__button').click
     expect(page).to_not have_selector('a', text: 'Sign Up')
   end
 
@@ -17,14 +17,14 @@ feature 'session' do
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
-    click_button 'Sign in'
+    find('.retrospective-form__button').click
     expect(page).to_not have_selector('a', text: 'Sign In')
   end
 
   scenario 'success sign out' do
     sign_in user
     visit root_path
-    click_link 'Sign Out'
-    expect(page).to have_selector('a', text: 'Sign In')
+    find('.navigation__item--sign-out').click
+    expect(page).to have_selector('a', text: 'Sign up')
   end
 end
