c0da8b97a0168331215374613ea8ebf5d8910e88
danny <danny.dev8@gmail.com>
2015-08-20 02:51:57 +0900

    replace React.js

---
+++ app/assets/javascripts/components/retrospective_label.js.jsx
@@ -0,0 +1,37 @@
+class RetrospectiveLabel extends React.Component {
+  destroy(event) {
+    let id = this.refs.label.getDOMNode().dataset.id;
+    this.props.destroy(id);
+    event.stopPropagation();
+  }
+
+  edit(event) {
+    let id = this.refs.label.getDOMNode().dataset.id;
+    this.props.edit(event, id);
+    event.stopPropagation();
+  }
+
+  onDragStart(event) {
+    let id = this.refs.label.getDOMNode().dataset.id;
+    this.props.onDragStart(event, id);
+  }
+
+  onDragEnd(event) {
+    this.props.onDragEnd(event);
+  }
+
+  render() {
+    return (
+      <div className={'js-label retrospective__label retrospective__label--' + this.props.label.typ} onClick={this.edit.bind(this)} data-id={this.props.label.id} draggable="true" onDragStart={this.onDragStart.bind(this)} onDragEnd={this.onDragEnd.bind(this)} ref="label">
+        <div className="retrospective__label-content">
+          <div className="retrospective__label-header">
+            <i className="retrospective__label-remove fa fa-remove" onClick={this.destroy.bind(this)}></i>
+            <div className="retrospective__label-created-at">{this.props.label.created_at}</div>
+            <div className="retrospective__label-user-name">{this.props.label.user_name}</div>
+          </div>
+          <div className="retrospective__label-description">{this.props.label.description}</div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/components/retrospective_menu.js.jsx
@@ -0,0 +1,28 @@
+class RetrospectiveMenu extends React.Component {
+  addUser() {
+    let email = this.refs.email.getDOMNode().value;
+    if (email) {
+      this.props.addUser(email);
+      this.refs.email.getDOMNode().value = null;
+    }
+  }
+
+  render() {
+    let users = this.props.users.map((user) => {
+      return (
+        <div className="retrospective__user" key={user.id}>
+          <div className="fa fa-user">
+            <div className="retrospective__user-name">{user.name}</div>
+         </div>
+        </div>
+      )
+    });
+    return (
+      <div className="retrospective__menu">
+        <input className="retrospective__menu-email-form" type="email" placeholder="email" ref="email"></input>
+        <button className="retrospective__menu-email-form-button fa fa-user-plus" type="button" onClick={this.addUser.bind(this)}></button>
+        <div className="retrospective__users">{users}</div>
+      </div>
+    );
+  }
+}
