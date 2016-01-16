39745459f3fd0aa6861bb26e2a8f8871b6231c1b
danny <danny.dev8@gmail.com>
2016-01-16 19:09:39 +0900

    refactor order

--- app/models/retrospective.rb
+++ app/models/retrospective.rb
@@ -1,6 +1,6 @@
 class Retrospective < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
-  has_many :labels, ->{ order('position ASC') }, dependent: :destroy
+  has_many :labels, ->{ order(position: :asc) }, dependent: :destroy
   has_many :retrospective_users, dependent: :destroy
   has_many :users, through: :retrospective_users
 
