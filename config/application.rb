aca9696f4bb5cdaab4e7eb03cd766d0dfc568a9f
danny <danny.dev8@gmail.com>
2016-05-03 00:38:29 +0900

    add locale

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,10 @@ module Kptboard
 
     config.time_zone = 'Tokyo'
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.enforce_available_locales = true
-    config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:en, :ja]
 
     config.generators do |g|
       g.fixture_replacement :fabrication
