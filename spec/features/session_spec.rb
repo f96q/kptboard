2882c36afabfae5effdd7bc3307a5529b7b9c695
danny <danny.dev8@gmail.com>
2016-05-03 02:02:52 +0900

    add devise locale

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -5,26 +5,26 @@ feature 'session' do
 
   scenario 'success sign up' do
     visit new_user_registration_path
-    fill_in 'Email', with: 'test@example.com'
-    fill_in 'Name', with: 'test'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
+    fill_in :user_email, with: 'test@example.com'
+    fill_in :user_name, with: 'test'
+    fill_in :user_password, with: 'password'
+    fill_in :user_password_confirmation, with: 'password'
     find('.js-sign-up').click
-    expect(page).to_not have_selector('a', text: 'Sign Up')
+    expect(page).to_not have_text I18n.t('devise.registrations.new.sign_up')
   end
 
   scenario 'success sign in' do
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: 'password'
     find('.js-sign-in').click
-    expect(page).to_not have_selector('a', text: 'Sign In')
+    expect(page).to_not have_text I18n.t('devise.sessions.new.sign_in')
   end
 
   scenario 'success sign out' do
     sign_in user
     visit root_path
     find('.js-sign-out').click
-    expect(page).to have_selector('a', text: 'Sign up')
+    expect(page).to have_text I18n.t('devise.registrations.new.sign_up')
   end
 end
--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -1,8 +1,8 @@
 module FeatureHelper
   def sign_in(user, password = 'password')
     visit new_user_session_path
-    fill_in 'Email',    with: user.email
-    fill_in 'Password', with: password
-    click_button 'Sign in'
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: password
+    find('.js-sign-in').click
   end
 end
