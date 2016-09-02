22bc43931ad1f4f73ca1203f16a4dccc3a3fd833
danny <danny.dev8@gmail.com>
2016-09-02 22:00:38 +0900

    fix set NODE_ENV

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './frontend/javascripts/application.js',
   output: {
@@ -15,5 +17,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
