e3d8f5af32af50a2df0b120fa306714b95e543e8
danny <danny.dev8@gmail.com>
2017-06-06 01:43:43 +0900

    Replase es2015 + stage0

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['react', 'latest'] }
+          options: { presets: ['es2015', 'stage-0', 'react'] }
         }]
       }
     ]
