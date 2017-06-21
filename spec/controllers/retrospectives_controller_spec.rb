3ba7f1ab3b9a43cda5bdc2722b3d084b5b623c92
danny <danny.dev8@gmail.com>
2017-06-21 23:07:10 +0900

    Add Reviews Apps data

--- spec/controllers/retrospectives_controller_spec.rb
+++ spec/controllers/retrospectives_controller_spec.rb
@@ -4,8 +4,8 @@ describe RetrospectivesController do
   describe 'export' do
     render_views
 
-    let(:user) { Fabricate :user }
-    let!(:retrospective) { Fabricate :retrospective }
+    let(:user) { Fabricate :user, name: 'test' }
+    let!(:retrospective) { Fabricate :retrospective, title: 'retrospective' }
     let!(:keep_label) { Fabricate :label, kind: 'keep', retrospective: retrospective, user: user, description: 'description1', created_at: '2014-04-26' }
     let!(:problem_label) { Fabricate :label, kind: 'problem', retrospective: retrospective, user: user, description: 'description2', created_at: '2014-04-26' }
     let!(:try_label) { Fabricate :label, kind: 'try', retrospective: retrospective, user: user, description: 'description3', created_at: '2014-04-26' }
