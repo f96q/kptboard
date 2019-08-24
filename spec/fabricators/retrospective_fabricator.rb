62df3c31b3460bfc63923e2503d63569e44ffdc7
danny <danny.dev8@gmail.com>
2019-08-25 00:59:17 +0900

    Update ruby 2.6.3

--- spec/fabricators/retrospective_fabricator.rb
+++ spec/fabricators/retrospective_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:retrospective) do
   title { Faker::Book.unique.title }
   created_at {
     time = Time.current
-    Faker::Time.between(1.years.ago(time), time)
+    Faker::Time.between(from: 1.years.ago(time), to: time)
   }
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:user) do
-  name { Faker::Pokemon.unique.name }
+  name { Faker::Games::Pokemon.unique.name }
   email { Faker::Internet.unique.safe_email }
   password 'password'
 end
